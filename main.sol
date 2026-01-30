7575160d5d0119600db9fdf9dcd72c7f2a675b08
blockaholic <143205778+nftk3vin@users.noreply.github.com>
2026-01-30 09:32:45 -0600

    Update main.sol

---
+++ main.sol
@@ -0,0 +1,7 @@
+contract X{
+uint256 private a;
+mapping(address=>uint256) private b;
+function f(uint256 x) external returns(uint256){
+a^=x+(a<<1);
+b[msg.sender]=(b[msg.sender]+a)^x;
+return uint256(keccak256(abi.encodePacked(a,b[msg.sender],block.number)));

f2f077cd2d142d379e00253081b43b87727cd88e
blockaholic <143205778+nftk3vin@users.noreply.github.com>
2026-01-30 09:33:35 -0600

    Update main.sol

--- main.sol
+++ main.sol
@@ -5,3 +5,9 @@ function f(uint256 x) external returns(uint256){
 a^=x+(a<<1);
 b[msg.sender]=(b[msg.sender]+a)^x;
 return uint256(keccak256(abi.encodePacked(a,b[msg.sender],block.number)));
+}
+function g() external view returns(uint256){
+return (a>>2)^b[msg.sender];
+}
+}
+
